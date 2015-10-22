3ca74a686e8481ab1d7aae465683cb6d91c34362
Soulao999 <Soulao999@gmail.com>
2015-10-22 12:17:30 +0200

    Base
    Base du projet

---
+++ app/src/main/java/fr/joffre/tpe/joffrecourse/MainActivity.java
@@ -0,0 +1,237 @@
+package fr.joffre.tpe.joffrecourse;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, View.OnClickListener, android.location.LocationListener {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
+    private CharSequence mTitle;
+
+    private Button b;
+    private TextView tv;
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    private float accuracy;
+    static LocationManager lm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+        b = (Button) findViewById(R.id.startbutton);
+        b.setOnClickListener(this);
+        tv = (TextView) findViewById(R.id.Acceuil_textView);
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria critere = new Criteria();
+        critere.setAccuracy(Criteria.ACCURACY_FINE);
+        critere.setAltitudeRequired(true);
+        critere.setBearingRequired(true);
+        critere.setCostAllowed(false);
+        critere.setPowerRequirement(Criteria.POWER_HIGH);
+        critere.setSpeedRequired(true);
+        try {
+            Log.i("Localisation", lm.getBestProvider(critere, true));
+        } catch (NullPointerException e) {
+            Toast.makeText(this, R.string.Accueil_erreurLocalisation, Toast.LENGTH_SHORT).show();
+        }
+        String best = lm.getBestProvider(critere, false);
+        lm.requestLocationUpdates(best, 10000, 1, this);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 1, this);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 1, this);
+        Log.i("Localisation GPS", "Start localisation" + String.valueOf(lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)));
+        Log.i("Localisation NETWORK", "Start localisation" + String.valueOf(lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)));
+        Log.i("Localisation BEST", "Start localisation" + String.valueOf(lm.getLastKnownLocation(best)));
+    }
+
+    @Override
+    public void onDestroy(){
+       super.onDestroy();
+       lm.removeUpdates(this);
+    }
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        altitude = location.getAltitude();
+        accuracy = location.getAccuracy();
+        Log.i("Localisation", "Lat: " + latitude + " Lon: " + longitude + " Alt: " + altitude + " acc: "+ accuracy);
+        tv.setText("Localisation: Lat: "+ latitude + " Lon: " + longitude + " Alt: " + altitude + " acc: "+ accuracy);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.i("Localisation"," Statut changed");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.i("Localisation","Enabled");
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.i("Localisation","Disabled");
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/fr/joffre/tpe/joffrecourse/Options.java
@@ -0,0 +1,176 @@
+package fr.joffre.tpe.joffrecourse;
+
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static java.lang.String.*;
+
+public class Options extends AppCompatActivity
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, View.OnClickListener {
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private CharSequence mTitle;
+
+    private Button b = null;
+    private EditText editTextAge = null;
+    private EditText editTextPoids = null;
+    private EditText editTextTaille = null;
+    public static final String AGE = "AGE";
+    public static final String POIDS = "POIDS";
+    public static final String TAILLE = "TAILLE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        editTextPoids = (EditText) findViewById(R.id.editTextPoids);
+        editTextTaille = (EditText) findViewById(R.id.editTextTaille);
+        b = (Button) findViewById(R.id.buttonOptionValider);
+        b.setOnClickListener(this);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        Log.i("préférences", String.valueOf(preferences.getAll()));
+        try{
+            editTextAge.setText(String.valueOf(preferences.getInt(AGE, 0)));
+            editTextPoids.setText(String.valueOf(preferences.getInt(POIDS, 0)));
+            editTextTaille.setText(String.valueOf(preferences.getInt(TAILLE, 0)));
+            Log.i("Preferences", "Préférences chargées");
+        }
+        catch(Resources.NotFoundException e){
+            Log.i("Preferences", "Les préférences sont introuvables");
+        }
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .commit();
+    }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v){
+        try{
+            if(Integer.parseInt(valueOf(editTextAge.getText())) >= 150){ Toast.makeText(this, R.string.Options_erreurAgePlus, Toast.LENGTH_LONG).show();}
+            else if(Integer.parseInt(valueOf(editTextAge.getText())) <= 5){Toast.makeText(this, R.string.Options_erreurAgeMoins, Toast.LENGTH_LONG).show();}
+            else if(Integer.parseInt(valueOf(editTextPoids.getText())) <= 20){ Toast.makeText(this, R.string.Options_erreurPoids, Toast.LENGTH_LONG).show();}
+            else if(Integer.parseInt(valueOf(editTextTaille.getText())) <= 20){ Toast.makeText(this, R.string.Options_erreurTailleMoins, Toast.LENGTH_LONG).show();}
+            else if(Integer.parseInt(valueOf(editTextTaille.getText())) >= 400){ Toast.makeText(this, R.string.Options_erreurTaillePlus, Toast.LENGTH_LONG).show();}
+             else {
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putInt(AGE, Integer.parseInt(valueOf(editTextAge.getText())));
+                editor.commit();
+                editor.putInt(POIDS, Integer.parseInt(valueOf(editTextPoids.getText())));
+                editor.commit();
+                editor.putInt(TAILLE, Integer.parseInt(valueOf(editTextTaille.getText())));
+                editor.commit();
+                Toast.makeText(this, R.string.Options_enregistre, Toast.LENGTH_LONG).show();
+                Log.i("préférences", String.valueOf(preferences.getAll()));
+            }
+
+        }
+        catch (NumberFormatException e){
+            Toast.makeText(this, R.string.Options_erreur, Toast.LENGTH_LONG).show();
+        }
+        }
+
+
+
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+
+    }
+}
+
+
